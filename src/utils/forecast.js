ee1dc2ce016fb426fb78e7c39abce8c51fb7886e
Jim Bird <goforbird@gmail.com>
2021-01-20 21:05:29 -0500

    Added more data to the weather app results.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,21 @@
 const request = require('request')
 
+const parseTime = (apiTime, offset) => {
+    const time = apiTime.split(' ')
+    const hour = time[0].split(':')
+    const modTime = parseInt(hour[0]) + parseInt(offset)
+    if (modTime < 0) {
+        const newTime = parseInt(hour[0]) + parseInt(offset) + 24
+        return newTime.toString() + ':' + hour[1]
+    } else if (modTime > 24) {
+        const newTime = parseInt(hour[0]) + parseInt(offset) - 24
+        return newTime.toString() + ':' + hour[1]
+    } else {
+        const newTime = parseInt(hour[0]) + parseInt(offset)
+        return newTime.toString() + ':' + hour[1]
+    }
+}
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=e1ae7a5d7646724be26f23f445860345&query=' + latitude + ',' + longitude + '&units=f'
     request({url, json: true}, (error, {body}) => {
@@ -8,7 +24,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Server returned an error. Did you provide a valid location?')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            const timePub = parseTime(body.current.observation_time, body.location.utc_offset)
+            console.log(timePub)
+            callback(undefined, 'As of ' + timePub + ' local time, the weather is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees F out. It feels like ' + body.current.feelslike + ' degrees F. The humiditiy is ' + body.current.humidity + '%.')
         }
     })
 }
